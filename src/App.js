74c5d49abfc5ea1711d808f8becc478be6798165
Wojtek <wwojciechjarocki@gmail.com>
2023-08-02 08:41:48 +0200

    add browserrouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage"
+import ActorPage from "./pages/ActorPage"
+import MoviePage from "./pages/MoviePage"
+import PrivacyPage from "./pages/PrivacyPage"
+
 
 const App = () => {
   return (
-    <div>App</div>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/actor" element={<ActorPage/>} />
+        <Route path="/movie" element={<MoviePage/>}/>
+        <Route path="/privacy" element={<PrivacyPage/>} /> 
+      </Routes>
+    </BrowserRouter>
   )
 }
 
