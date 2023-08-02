552e3be9de33a521afec932229b4895fdba1f948
dawtom97 <daw.tomczyk1997@gmail.com>
2023-08-02 10:40:39 +0200

    custom hook for fetching movies

---
+++ src/hooks/useFetchMovies.js
@@ -0,0 +1,45 @@
+import { useToast } from "@chakra-ui/react";
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+const KEY = "bb73a88521ab379e017b9a1df971b994"
+
+const useFetchMovies = (query) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+    const toast = useToast()
+
+    useEffect(()=>{
+       getData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
+    const getData = async () => {
+
+        try {
+            const response = await axios.get(`https://api.themoviedb.org/3/movie/${query}?api_key=${KEY}`);
+            setData(response.data)
+        } catch (error) {
+            console.log(error)
+            setError("Wystąpił błąd")
+            toast({
+                title: 'Nie udało się pobrać danych',
+                description: "Skontaktuj się z administratorem strony",
+                status: 'error',
+                duration: 3000,
+                isClosable: true,
+            })
+        } finally {
+            setLoading(false)
+        }
+
+  
+    }
+
+    return {data, loading, error}
+
+
+}
+
+export default useFetchMovies
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import Header from '../components/Header'
-import Footer from "../components/Footer"
 import MainTemplate from '../templates/MainTemplate'
+import useFetchMovies from '../hooks/useFetchMovies'
 
 
 const HomePage = () => {
+
+  const popular = useFetchMovies("popular");
+  const playing = useFetchMovies("now_playing");
+
+  console.log(popular.data)
+  console.log(playing.data)
+
   return (
     <MainTemplate>
+      {popular.loading ? <p>Wczytywanie</p> : null}
       <h1>Homepage</h1>     
     </MainTemplate>
   )
