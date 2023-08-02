d0f51e490411eb3b3f6db23acd1164ec6b115d01
dawtom97 <daw.tomczyk1997@gmail.com>
2023-08-02 11:57:00 +0200

    popular movies section

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,31 +1,79 @@
-import { Box, CircularProgress, CircularProgressLabel, Heading, Image, SimpleGrid } from '@chakra-ui/react'
-import React from 'react'
+import {
+  Box,
+  CircularProgress,
+  CircularProgressLabel,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
 
-const Movies = ({ items }) => {
-	console.log(items)
-	return (
-		<Box maxW={'8xl'} mx='auto' px={{ base: 4 }}>
-			<SimpleGrid columns={{ sm: 2, md: 3, lg: 4, xl: 5 }} spacing={10}>
-				{items.map(item => (
-					<Box key={item.id} bg='white'>
-						<Image src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} alt='Dan Abramov' />
+const Movies = ({ items,title }) => {
+  console.log(items);
+  return (
+    <Box my={5} maxW={"8xl"} mx="auto" px={{ base: 4 }}>
+      <Heading mb={4} mt={5} as="h2" size="lg" noOfLines={1}>
+        {title}
+      </Heading>
 
+      <Swiper
+        spaceBetween={15}
+        slidesPerView={1.5}
+        breakpoints={{
+          640: {
+            slidesPerView: 2.3,
+          },
+          768: {
+            slidesPerView: 4.3,
+          },
+          1024: {
+            slidesPerView: 7.5,
+          },
+        }}
+        // onSlideChange={() => console.log("slide change")}
+        // onSwiper={(swiper) => console.log(swiper)}
+      >
+        {items.map((item) => (
+          <SwiperSlide key={item.id}>
+            <Box position={"relative"} bg="white">
+              <Image
+                borderRadius={15}
+                src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                alt="Dan Abramov"
+              />
 
-						<CircularProgress value={item.vote_average*10} color='green.400'>
-							<CircularProgressLabel>{item.vote_average}</CircularProgressLabel>
-						</CircularProgress>
+              <CircularProgress
+                position={"absolute"}
+                bg="gray.900"
+                borderRadius="50%"
+                bottom="50px"
+                left="10px"
+                value={item.vote_average * 10}
+                color="green.400"
+                size="40px"
+              >
+                <CircularProgressLabel color="white" fontWeight={700} fontSize={13}>
+                  {item.vote_average}
+                </CircularProgressLabel>
+              </CircularProgress>
 
-						<Heading as='h2' size='lg' noOfLines={1}>
-							{item.title}
-						</Heading>
-						<Heading as='p' size='xs' noOfLines={1}>
-							{item.release_date}
-						</Heading>
-					</Box>
-				))}
-			</SimpleGrid>
-		</Box>
-	)
-}
+              <Heading mt={7} as="h2" size="sm" noOfLines={1}>
+                {item.title}
+              </Heading>
+              <Text color="gray.400" fontSize="12px">
+                {item.release_date}
+              </Text>
+            </Box>
+          </SwiperSlide>
+        ))}
 
-export default Movies
+      </Swiper>
+    </Box>
+  );
+};
+
+export default Movies;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,7 +11,7 @@ const HomePage = () => {
 
   return (
     <MainTemplate>
-       <Movies items={popular.data}/>
+       <Movies title="Najpopularniejsze" items={popular.data}/>
     </MainTemplate>
   );
 };
