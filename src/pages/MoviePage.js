a7438b7dfd74718764be0847e90c5d6a9e3195dd
dawtom97 <daw.tomczyk1997@gmail.com>
2023-08-03 09:12:46 +0200

    fetch movie by id

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MainTemplate from '../templates/MainTemplate'
 import { useParams } from 'react-router-dom'
 import useFetchMovies from '../hooks/useFetchMovies'
+import axios from 'axios'
 
 const MoviePage = () => {
 	const params = useParams()
 	const itemid = params.url.replace(/\D/g, '')
-	const itemids = useFetchMovies(itemid);
-	console.log(itemids)
+	const [movie, setMovie] = useState()
 
+	useEffect(()=>{
+      getMovie()
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	},[])
+	
+	const getMovie = async () => {
+		try {
+			const response = await axios.get(`https://api.themoviedb.org/3/movie/${itemid}?api_key=${process.env.REACT_APP_API_KEY}`);
+            setMovie(response.data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	console.log(movie)
   
 	return (
 		<MainTemplate>
