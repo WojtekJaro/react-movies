97acee30ddc982496152c03caa4dbb3bae843a0d
Wojtek <wwojciechjarocki@gmail.com>
2023-08-03 09:08:39 +0200

    movie page

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import MainTemplate from '../templates/MainTemplate'
+import { useParams } from 'react-router-dom'
+import useFetchMovies from '../hooks/useFetchMovies'
 
 const MoviePage = () => {
-  return (
-    <MainTemplate>
-      <h1>Movie</h1>
-    </MainTemplate>
-  )
+	const params = useParams()
+	const itemid = params.url.replace(/\D/g, '')
+	const itemids = useFetchMovies(itemid);
+	console.log(itemids)
+
+  
+	return (
+		<MainTemplate>
+			<h1>Movie</h1>
+		</MainTemplate>
+	)
 }
 
 export default MoviePage
