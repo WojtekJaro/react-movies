5344a4b855ebb6a7a2ec1e056813254c65943e09
Wojtek <wwojciechjarocki@gmail.com>
2023-11-12 20:44:38 +0100

    color extractor bugfix

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,133 +1,106 @@
-import React, { useEffect, useState } from "react";
-import MainTemplate from "../templates/MainTemplate";
-import { useParams } from "react-router-dom";
-import useFetchMovies from "../hooks/useFetchMovies";
-import axios from "axios";
+import React, { useEffect, useState } from 'react'
+import MainTemplate from '../templates/MainTemplate'
+import { useParams } from 'react-router-dom'
+import useFetchMovies from '../hooks/useFetchMovies'
+import axios from 'axios'
 import {
-  Box,
-  CircularProgress,
-  CircularProgressLabel,
-  Flex,
-  Grid,
-  GridItem,
-  Heading,
-  Image,
-  Text,
-} from "@chakra-ui/react";
-
-
+	Box,
+	CircularProgress,
+	CircularProgressLabel,
+	Flex,
+	Grid,
+	GridItem,
+	Heading,
+	Image,
+	Text,
+} from '@chakra-ui/react'
 
 const MoviePage = () => {
-  const params = useParams();
-  const itemid = params.url.replace(/\D/g, "");
-  const [movie, setMovie] = useState();
-  const [colors, setColors] = useState([])
+	const params = useParams()
+	const itemid = params.url.replace(/\D/g, '')
+	const [movie, setMovie] = useState()
+	const [colors, setColors] = useState([])
 
+	useEffect(() => {
+		getMovie()
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 
-  useEffect(() => {
-    getMovie();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	const handleGetColors = clr => {
+		setColors(clr.map(c => c.join(', ')))
+	}
 
-  const handleGetColors = (clr) => {
-   setColors(clr.map((c) => c.join(", ")))
-  }
+	const getMovie = async () => {
+		try {
+			const response = await axios.get(
+				`https://api.themoviedb.org/3/movie/${itemid}?api_key=${process.env.REACT_APP_API_KEY}&language=pl`
+			)
+			setMovie(response.data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	if (!movie && colors.length === 0) {
+		return
+	}
 
-  const getMovie = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.themoviedb.org/3/movie/${itemid}?api_key=${process.env.REACT_APP_API_KEY}&language=pl`
-      );
-      setMovie(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  if (!movie && colors.length === 0) {
-    return;
-  }
+	console.log(colors)
 
-  console.log(colors)
+	return (
+		<MainTemplate>
+			<Box >
+				<Box 
+					bgSize='cover'
+					bgPosition='center'
+          bgColor='red'
+					bgImage={`linear-gradient(to right, rgb(${colors[5]}) calc((50vw - 170px) - 340px), rgb(${colors[0]}) 100%),url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`}
+					maxWidth={'100%'}
+					my={5}
+					py={{ base: 9 }}
+					px={{ base: 4 }}>
+					<Grid color='white' maxW={'8xl'} mx='auto' templateColumns='300px 1fr 1fr' gap={10}>
+						<GridItem rowSpan={2}>
+							<Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt='Dan Abramov' borderRadius={8} />
+						</GridItem>
 
-  return (
-    <MainTemplate>
+						<GridItem colSpan={1}>
+							<Heading>{movie.title}</Heading>
+							<Text>
+								{movie.release_date} ({movie.original_language.toUpperCase()}) &#x2022;{' '}
+								{movie.genres.map(item => (
+									<span>{item.name}, </span>
+								))}{' '}
+								&#x2022; {movie.runtime}{' '}
+							</Text>
 
-         {/* <Image rgb  getColors={handleGetColors}>
-          <img
-            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-            style={{ display:"none" }}
-            alt=""
-          />
-        </Image>
-		 */}
-      <Box
-	  bgSize="cover"
-	    bgPosition="center"
-        bgImage={`linear-gradient(to right, rgb(${colors[5]}) calc((50vw - 170px) - 340px), rgb(${colors[0]}) 100%),url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`}
-        maxWidth={"100%"}
-        my={5}
-        py={{ base: 9 }}
-        px={{ base: 4 }}
-      >
-        <Grid
-          color="white"
-          maxW={"8xl"}
-          mx="auto"
-          templateColumns="300px 1fr 1fr"
-          gap={10}
-        >
-          <GridItem rowSpan={2}>
-            <Image
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              alt="Dan Abramov"
-			  borderRadius={8}
-            />
-          </GridItem>
+							<Flex mt='30px' align='center' size='100px' gap={3}>
+								<CircularProgress
+									bg='gray.900'
+									borderRadius='50%'
+									value={movie.vote_average * 10}
+									color='blue.400'
+									size='60px'>
+									<CircularProgressLabel color='white' fontWeight={700} fontSize={18}>
+										{movie.vote_average.toFixed(1)}
+									</CircularProgressLabel>
+								</CircularProgress>
+								<Text fontWeight={700} textAlign='center'>
+									Ocena użytkowników
+								</Text>
+							</Flex>
+						</GridItem>
 
-          <GridItem colSpan={1}>
+						<GridItem colSpan={2}>
+							<Text fontSize={22} fontWeight={700}>
+								Opis
+							</Text>
+							<Text>{movie.overview}</Text>
+						</GridItem>
+					</Grid>
+				</Box>
+			</Box>
+		</MainTemplate>
+	)
+}
 
-            <Heading>{movie.title}</Heading>
-            <Text>
-              {movie.release_date} ({movie.original_language.toUpperCase()})
-              &#x2022;{" "}
-              {movie.genres.map((item) => (
-                <span>{item.name}, </span>
-              ))}{" "}
-              &#x2022; {movie.runtime}{" "}
-            </Text>
-
-            <Flex mt="30px" align="center" size="100px" gap={3}>
-              <CircularProgress
-                bg="gray.900"
-                borderRadius="50%"
-                value={movie.vote_average * 10}
-                color="blue.400"
-                size="60px"
-              >
-                <CircularProgressLabel
-                  color="white"
-                  fontWeight={700}
-                  fontSize={18}
-                >
-                  {movie.vote_average.toFixed(1)}
-                </CircularProgressLabel>
-              </CircularProgress>
-              <Text fontWeight={700} textAlign="center">
-                Ocena użytkowników
-              </Text>
-            </Flex>
-          </GridItem>
-
-          <GridItem colSpan={2}>
-            <Text fontSize={22} fontWeight={700}>
-              Opis
-            </Text>
-            <Text>{movie.overview}</Text>
-          </GridItem>
-        </Grid>
-      </Box>
-    </MainTemplate>
-  );
-};
-
-export default MoviePage;
+export default MoviePage
